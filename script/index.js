f8d52511bd4f9a1bd5f3e71fcdbcc4a94efad937
Treasure Uzoma <123127790+TreasureUzoma@users.noreply.github.com>
2024-08-14 08:33:32 +0100

    Add files via upload

--- script/index.js
+++
@@ -1,8 +0,0 @@
-const isDesktop = window.matchMedia("(min-width: 769px)");
-
-
-window.onresize = function() {
-  if (isDesktop.matches) {
-    alert("This website is not available for desktop view. Please switch to mobile view for a more comfortable view.");
-  }
-};
